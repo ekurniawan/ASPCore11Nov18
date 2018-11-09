bc9404cc50e77b5ba6c676f7fa8ee3ad5372db88
Erick Kurniawan <erick.kurniawan@gmail.com>
2018-11-09 11:26:03 +0700

    modifikasi about

--- SampleASPCore/Controllers/HomeController.cs
+++ SampleASPCore/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace SampleASPCore.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Azure Deployment Center";
 
             return View();
         }
